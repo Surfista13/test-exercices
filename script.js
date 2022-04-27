722984e9ecec86ad6d99d7ee5fe2b644bcf88783
BGG44 <brunogar@laposte.net>
2022-04-27 21:15:07 +0200

    test modules ES6

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-function boxChoice () {
-    let numBoxHaz = Math.floor(Math.random() * 4)+1;
-    return `box${numBoxHaz}`;
-};
-function colorChoice () {
-    let num1 = Math.floor(Math.random() * 254)+1;
-    let num2 = Math.floor(Math.random() * 254)+1;
-    let num3 = Math.floor(Math.random() * 254)+1;
-    return `rgb(${num1},${num2},${num3})`;
-};
+import {colorChoice, boxChoice} from './module'
 
 function colorChange () {
     let box = boxChoice ();
