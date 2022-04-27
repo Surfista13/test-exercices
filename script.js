a9eb9b4eda4a0c42a768feefad99735cc18a9282
BGG44 <brunogar@laposte.net>
2022-04-27 22:08:53 +0200

    ff

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function colorChange () {
     let box = boxChoice ();
     let color2 = colorChoice ();
     document.getElementById(box).style.background = color2;
-    document.getElementById(box).innerHTML = color2;
+    document.getElementById(box).innerHTML = 'test';
 }
 
 let contID = document.getElementById('content');
