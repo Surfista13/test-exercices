72090dfd58f1957154e42e59011eaf7dfa053a0b
BGG44 <brunogar@laposte.net>
2022-05-03 09:55:08 +0200

    API FETCH

--- script.js
+++ script.js
@@ -1,47 +1,11 @@
-const listePeople = [['Pierre', 25], ['Bruno', 40]]
+const jsonData1 = '{ "book": { "name": "JSON Primer", "price": 29.99, "inStock": true, "rating": null } }';
+ 
+const jsObject1 = JSON.parse(jsonData1);
+ 
+console.log(jsObject1);
 
-const nameWinnerchoice = () => {
-    return new Promise ((resolve, reject) => {
-        console.log('Time to choose the winner');
-        setTimeout(() =>{
-            let winNum = Math.floor(Math.random()*100);
+const jsObject = { book: 'JSON Primer', price: 29.99, inStock: true, rating: null };
+const jsonData = JSON.stringify(jsObject);
+console.log(jsonData)
 
-            if (winNum > 1) {
-                reject('Pas de gagnant')
-            } else {
-                let winner = listePeople[winNum][0];
-                resolve(winner);  
-            }
-
-        }, 1000)       
-    })
-}
-
-const ageWinnerChoice = (winnerName) => {
-    return new Promise ((resolve, reject) => {
-        console.log(`What is the age of ${winnerName} ?`)
-        setTimeout(() => {
-            for (let i = 0; i < 2; i++) {
-                if(listePeople[i][0] == winnerName) {
-                    let winnerAge = listePeople[i][1];
-                    resolve(winnerAge);
-                }
-            }
-        }, 1000)
-    })
-}
-
-
-async function asyncWinner() {
-    try {        
-        let winnerNameaff = await nameWinnerchoice();
-        console.log(winnerNameaff);
-        let winnerAgeff = await ageWinnerChoice(winnerNameaff);
-        console.log(winnerAgeff);
-    } catch (err) {
-        console.log(err);
-    }
-    
-}
-
-asyncWinner()
+const answer = fetch('https://randomuser.me/api/');
